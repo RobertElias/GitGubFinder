df6fd33e98754df3f97c26e467ce371ab0100214
RobertElias <robert.elias@live.com>
2020-01-03 21:18:17 -0800

    updated UserItem.js

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,33 +1,28 @@
-import {Link} from 'react-router-dom'
-import PropTypes from 'prop-types'
-import React from 'react'
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import React from 'react';
 
-const UserItem = ({user: {login, avatar_url, html_url}}) => {
-
-
-        return (
-            <div className="card and text-center">
-            <img 
-            src={avatar_url}
-            alt="" 
-            className="round-img" 
-            style={{width: '60px'}}/>
-                <h3>{login}</h3>
-                <div>
-                <Link
-                to={`/user/${login}`} 
-                className="btn btn-dark btn-sm my-1" 
-                target="blank">
-                More
-                </Link>
-
-                </div>
-            </div>
-        );
-    };
-
-UserItem.propTypes = {
-user: PropTypes.object.isRequired
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className='card and text-center'>
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1'>
+          More
+        </Link>
+      </div>
+    </div>
+  );
 };
 
-export default UserItem
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
+};
+
+export default UserItem;
