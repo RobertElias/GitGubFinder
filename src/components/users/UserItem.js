646b667c600eb016dc447fa448bbc5d6fa683dca
RobertElias <robert.elias@live.com>
2020-01-03 21:46:17 -0800

    updates

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -13,7 +13,12 @@ const UserItem = ({ user: { login, avatar_url, html_url } }) => {
       />
       <h3>{login}</h3>
       <div>
-        <Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1'>
+        <Link
+          exact
+          path
+          to={`/user/${login}`}
+          className='btn btn-dark btn-sm my-1'
+        >
           More
         </Link>
       </div>
